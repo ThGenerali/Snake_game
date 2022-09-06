f55aa31794f56b97b29431c551a1d3b0e1a9ea9d
ThGenerali <thiagonmag@gmail.com>
2022-09-06 14:03:58 -0300

    ajustes feitos

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,10 +39,17 @@ function update (event){
 }
 
 function IniciarJogo(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
+    if(snake[0].y > 15*box && direction == "up") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "down") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].x == snake[i].y) {
+            clearInterval(jogo);
+            alert("Game Over!");
+        }
+    }
 
     criarBG();
     criarCobrinha();  
@@ -60,8 +67,8 @@ function IniciarJogo(){
         snake.pop();
     }
     else {
-        food.x = Math.floor(Math.random() * 15 + 1) * box;
-        food.y = Math.floor(Math.random() * 15 + 1) * box;
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
     }
     
     let newHead = {
